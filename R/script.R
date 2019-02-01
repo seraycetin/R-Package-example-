dd674733b3595fe9fc373307b2c9c66fc778f2c5
seraycetin <45455508+seraycetin@users.noreply.github.com>
2019-02-01 14:35:20 +0100

    Create script.R

---
+++ R/script.R
@@ -0,0 +1,32 @@
+hello <- function() {
+  print("Hello, world!")
+}
+
+square <- function(x){
+  return(x^2)
+}
+
+addition <- function(a, b){
+  return(a+b)
+}
+
+test_loop <- function(a, b){
+  .Call(c_add_f, a, b)
+}
+
+
+LLC_f <- function(x, l, a) {
+  if (!is.double(x)) {storage.mode(x) <- 'double'}
+  if (length(l) > 1 | length(a) > 1) {
+    stop("This toy example is restricted to singular values of the limit and attachment.")}
+  if (!is.double(l)) {storage.mode(l) <- 'double'}
+  if (!is.double(a)) {storage.mode(a) <- 'double'}
+  .Call(c_llc_f, x, l, a)
+}
+
+LLC_f2 <- function(x, l, a) {
+  if (!is.double(x)) {storage.mode(x) <- 'double'}
+  if (!is.double(l)) {storage.mode(l) <- 'double'}
+  if (!is.double(a)) {storage.mode(a) <- 'double'}
+  .Call(c_llc_f, x, l, a)
+}
